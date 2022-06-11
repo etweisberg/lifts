e6ddd564b2028ce6d701e5ebe4654ed5629652d4
Ethan Weisberg <eiweisberg.2718@gmail.com>
2022-06-10 21:11:30 -0400

    setting up get and create cloud functions

--- functions/functions/index.js
+++ functions/functions/index.js
@@ -1,9 +1,45 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", { structuredData: true });
+  response.send("Hello from Firebase!");
+});
+
+exports.getLifts = functions.https.onRequest((request, response) => {
+  admin
+    .firestore()
+    .collection("lifts")
+    .get()
+    .then((data) => {
+      let lifts = [];
+      data.forEach((doc) => {
+        lifts.push(doc.data());
+      });
+      return response.json(lifts);
+    })
+    .catch((err) => console.log(err));
+});
+
+exports.logLift = functions.https.onRequest((request, response) => {
+  if (request.method != "POST") {
+    return response.status(400).json({ error: "invalid request " });
+  }
+  const newLift = {
+    exercises: request.body.exercises,
+    userHandle: request.body.userHandle,
+    date: admin.firestore.Timestamp.fromDate(new Date()),
+  };
+  admin
+    .firestore()
+    .collection("lifts")
+    .add(newLift)
+    .then((doc) => {
+      response.json({ message: `document ${doc.id} created successfully` });
+    })
+    .catch((err) => {
+      response.status(500).json({ error: "something went wrong" });
+      console.error(err);
+    });
+});
